1b597a7ef0fd69e81bdd37cf2f74de7757ab883d
Zyflx <ezekielxroman@gmail.com>
2025-07-18 13:58:45 -0400

    allow converter to do the vice versa

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Psych Character To V-Slice Converter
 
-Converts Psych Engine character JSON's to V-Slice compatible character JSON's.
+Converts Psych Engine character JSON's to V-Slice compatible character JSON's and vice versa.
 
 # Usage
 
@@ -11,6 +11,12 @@ Just run the exe and thats it lol.
 Do `haxe build.hxml` in the command line.
 The output executable is located in "output/bin".
 
+# Conversion Modes
+
+PSYCH_TO_VSLICE and VSLICE_TO_PSYCH
+
 # Notes
 
-This converter does not support multi-sparrow psych characters.
+For Psych Engine to V-Slice conversion, there is currently no support for multi-sparrow Psych characters.
+
+V-Slice to Psych does support multi-sparrow V-Slice characters.
--- src/Main.hx
+++ src/Main.hx
@@ -6,6 +6,14 @@ import sys.io.File;
 
 using StringTools;
 
+/**
+ * Abstract which denotes the different conversion modes available.
+ */
+enum abstract ConvertMode(String) from String to String {
+    var PSYCH_TO_VSLICE = 'PSYCH_TO_VSLICE';
+    var VSLICE_TO_PSYCH = 'VSLICE_TO_PSYCH';
+}
+
 class Main {
     /**
      * Number which represents a error.
@@ -19,6 +27,25 @@ class Main {
 
     public static function main():Void {
         final args: Array<String> = Sys.args();
+        final convertMode: ConvertMode = awaitInput('Convert Mode:');
+
+        switch(convertMode) {
+            case ConvertMode.PSYCH_TO_VSLICE:
+                psychToVSlice(args);
+            case ConvertMode.VSLICE_TO_PSYCH:
+                vsliceToPsych(args);
+            case _:
+                closeWithMessage('No Conversion Method Specified. Closing...', 2, FALIURE);
+        }
+
+        closeWithMessage('Character Successfully Converted!');
+    }
+
+    /**
+     * Converts a Psych Engine character JSON to a V-Slice compatible character JSON.
+     * @param args List of arguments that were passed in (if any).
+     */
+    private static function psychToVSlice(args: Array<String>):Void {
         var fileName: String = args.length == 0 ? awaitInput('Psych Character JSON:') : args[0].trim();
 
         if(Path.extension(fileName) == '')
@@ -38,35 +65,36 @@ class Main {
         final characterImage: String = psychJSON.image.split(',')[0];
 
         final vsliceJSON: Map<String, Dynamic> = [
-            'version' => '1.0.0',
+            'version' => '1.0.1',
             'name' => characterName,
             'renderType' => 'sparrow',
             'assetPath' => characterImage,
-            'scale' => psychJSON.scale,
+            'scale' => psychJSON.scale ?? 1.0,
             'healthIcon' => {
-                'id': psychJSON.healthicon,
-                'isPixel': psychJSON.no_antialiasing
+                'id': psychJSON.healthicon ?? 'face',
+                'isPixel': psychJSON.no_antialiasing ?? false
             },
-            'offsets' => psychJSON.position,
-            'cameraOffsets' => psychJSON.camera_position,
-            'isPixel' => psychJSON.no_antialiasing,
+            'offsets' => psychJSON.position ?? [0, 0],
+            'cameraOffsets' => psychJSON.camera_position ?? [0, 0],
+            'isPixel' => psychJSON.no_antialiasing ?? false,
             'danceEvery' => 2,
-            'singTime' => psychJSON.sing_duration,
+            'singTime' => psychJSON.sing_duration ?? 8.0, // V-Slice's default sing time is 8.
             'startingAnimation' => 'idle',
             'animations' => [],
-            'flipX' => psychJSON.flip_x
+            'flipX' => psychJSON.flip_x ?? false
         ];
 
         for(animation in cast(psychJSON.animations, Array<Dynamic>)) {
-            final anim: String = animation.anim;
+            var anim: String = animation.anim;
+            if(anim.endsWith('-loop')) anim = anim.replace('-loop', '-hold');
 
             vsliceJSON.get('animations').push({
                 name: anim,
                 prefix: animation.name,
                 offsets: animation.offsets,
-                looped: animation.loop,
-                frameRate: animation.fps,
-                frameIndices: animation.indices
+                looped: animation.loop ?? false,
+                frameRate: animation.fps ?? 24,
+                frameIndices: animation.indices ?? []
             });
 
             if(anim.startsWith('dance') && vsliceJSON.get('startingAnimation') == 'idle')
@@ -79,8 +107,66 @@ class Main {
 
         final fileContent: String = Json.stringify(vsliceJSON, null, '\t');
         File.saveContent('${Path.withoutExtension(fileName)} (converted).json', fileContent);
+    }
 
-        closeWithMessage('Character Successfully Converted.');
+    /**
+     * Converts a V-Slice character JSON to a Psych Engine compatible character JSON.
+     * @param args 
+     */
+    private static function vsliceToPsych(args: Array<String>):Void {
+        var fileName: String = args.length == 0 ? awaitInput('V-Slice Character JSON:') : args[0].trim();
+
+        if(Path.extension(fileName) == '')
+            fileName += '.json';
+
+        var vsliceJSON: Dynamic = null;
+
+        try {
+            vsliceJSON = Json.parse(File.getContent(fileName));
+        } catch(e: haxe.Exception) {
+            closeWithMessage('ERROR: Invalid JSON Data.', 2, FALIURE);
+        }
+
+        final psychJSON: Map<String, Dynamic> = [
+            'animations' => [],
+            'no_antialiasing' => vsliceJSON.isPixel ?? false,
+            'image' => vsliceJSON.assetPath,
+            'position' => vsliceJSON.offsets ?? [0, 0], 
+            'healthicon' => vsliceJSON.healthIcon != null ? vsliceJSON.healthIcon.id : 'face',
+            'flip_x' => vsliceJSON.flipX ?? false,
+            'healthbar_colors' => [255, 0, 0],
+            'camera_position' => vsliceJSON.cameraOffsets ?? [0, 0],
+            'sing_duration' => vsliceJSON.singTime ?? 6.1, // Psych Engine's default sing time is 6.1, similar to legacy base game and other legacy based engines.
+            'scale' => vsliceJSON.scale ?? 1.0
+        ];
+
+        final images: Array<String> = [];
+
+        for(animation in cast(vsliceJSON.animations, Array<Dynamic>)) {
+            var anim: String = animation.name;
+            if(anim.endsWith('-hold')) anim = anim.replace('-hold', '-loop');
+
+            psychJSON.get('animations').push({
+                offsets: animation.offsets,
+                loop: animation.looped ?? false,
+                anim: anim,
+                name: animation.prefix,
+                fps: animation.frameRate ?? 24,
+                indices: animation.frameIndices ?? []
+            });
+
+            if(animation.assetPath != null)
+                images.push(animation.assetPath);
+        }
+
+        for(i in 0...images.length) {
+            // Skip duplicate names
+            if((psychJSON.get('image') + '').contains(images[i])) continue;
+            psychJSON.set('image', psychJSON.get('image') + ((i != images.length - 1) ? ',' : '') + '${images[i]}');
+        }
+
+        final fileContent: String = Json.stringify(psychJSON, null, '\t');
+        File.saveContent('${Path.withoutExtension(fileName)} (converted).json', fileContent);
     }
 
     /**
